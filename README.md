fbb255be152dc51c8fcb7314a925724cec51a245
aurelie85 <aurelie85@users.noreply.github.com>
2014-05-24 18:50:54 +0200

    Update

--- README.md
+++ README.md
@@ -1,43 +1,5 @@
+The following files are in this repository in order to solute the project of "Getting and Cleaning Data Course Project" on Coursera.
 
-## Instructions
-
-* The R script called run_analysis.R does the following. 
-* 1. Merges the training and the test sets to create one data set.
-* 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-* 3. Uses descriptive activity names to name the activities in the data set
-* 4. Apropriately labels the data set with descriptive activity names. 
-* 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-Below, you will find the explication for the steps.
-  
-## 0. Data Preparation
-* Load packages that we would use
-* change the working directory, where to load the data
-* Download the data
-* Read the files
-
-## 1. Merges the training and the test sets to create one data set
-
-* Use the "rbind" function to merge the data
-* Merge then columns.
-* Create key on a data table
-
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement 
-
-* read the 'features.txt' file for the names of the variables which are mean and standard deviation
-* subset only measurements for the mean and standard deviation.
-* Convert the column numbers to a vector of variable names matching columns in the created file.
-* Subset these variables using variable names.
-
-## 3. Uses descriptive activity names to name the activities in the data set
-  
-* Read the file 'activity_labels.txt' to add the descriptive names to the activities.
-
-## 4. Apropriately labels the data set with descriptive activity names. 
-
-* Convert the activity data into human-readable factors
-* Merge activity name.
-
-## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.  
-  
-* generate the tidy data set by using the 'write.table' function
+the readme file
+run_analysis.R, which contains the R commands to get, transform and produce the tidy data set
+tidy.txt, the tidy data set
--- run_analysis.R
+++ run_analysis.R
@@ -1,158 +1,62 @@
-##################
-## Instructions ##
-##################
+## Read in the test data.
+testSet <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testLabels <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-#The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 1) a tidy data set as described below, 2) a link to a Github repository with your script for performing the analysis, and 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
+## Read the training data in the same way
 
-#One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
-
-#http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-#Here are the data for the project: 
-#https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-
-#You should create one R script called run_analysis.R that does the following. 
-#1. Merges the training and the test sets to create one data set.
-#2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-#3. Uses descriptive activity names to name the activities in the data set
-#4. Apropriately labels the data set with descriptive activity names. 
-#5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.  
-
-## 0. Data Preparation
-## Load packages that we would use
-
-packages <- c("data.table", "reshape2")
-sapply(packages, require, character.only=TRUE, quietly=TRUE)
-
-# change the working directory, where to load the data
-
-path <- setwd("~/Coursera/Data Science Specialization/3. Getting and Cleaning Data/Peer Assessments/Getting-and-Cleaning-Data-Project")
-path
-
-## Download the data
-
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-f <- "Dataset.zip"
-if (!file.exists(path)) {dir.create(path)}
-download.file(url, file.path(path, f), method = "curl")
-
-pathIn <- file.path(path, "UCI HAR Dataset")
-list.files(pathIn, recursive = TRUE)
-
-## Read the files
-
-dtSubjectTrain <- fread(file.path(pathIn, "train", "subject_train.txt"))
-dtSubjectTest <- fread(file.path(pathIn, "test", "subject_test.txt"))
-
-dtActivityTrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
-dtActivityTest <- fread(file.path(pathIn, "test", "Y_test.txt"))
-
-fileToDataTable <- function (f) {
-  df <- read.table(f)
-  dt <- data.table(df)
-}
-dtTrain <- fileToDataTable(file.path(pathIn, "train", "X_train.txt"))
-dtTest  <- fileToDataTable(file.path(pathIn, "test" , "X_test.txt" ))
+trainSet <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainLabels <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 
-## 1. Merges the training and the test sets to create one data set
+## Chek the features of the data
+features <- read.table("./UCI HAR Dataset/features.txt")
+#features
 
-dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-setnames(dtSubject, "V1", "subject")
-dtActivity <- rbind(dtActivityTrain, dtActivityTest)
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
+## Convert the features colums to character.
+featureVector <- as.character(features[,2])
 
-# Merge columns.
+## Use the function "grep" to find the mean and the standard deviation
+meanCols <- grep("mean", featureVector)
+sdCols <- grep("std", featureVector)
 
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
+## Combine both into inly one.
+allCols <- sort(c(meanCols,sdCols))
 
-# Set key.
+## Create the test subset
+subsetTestSet <- testSet[,allCols]
 
-setkey(dt, subject, activityNum)
+## Add the activity label and name columns
 
+subsetTestWithLabels <- cbind(testLabels,subsetTestSet)
+subsetTestWithNames <- cbind(subjectTest,subsetTestWithLabels)
 
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## Create the training subset.
+subsetTrainSet <- trainSet[,allCols]
 
-# Tips found in the forum: check int the 'features.txt' file for the names of the variables in `dt` which are mean and standard deviation.
+## Add the activity label and name columns.
+subsetTrainWithLabels <- cbind(trainLabels, subsetTrainSet)
+subsetTrainWithNames <- cbind(subjectTrain, subsetTrainWithLabels)
 
-dtFeatures <- fread(file.path(pathIn, "features.txt"))
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
+## Combine the two data frames into one.
+combinedDataset <- rbind(subsetTestWithNames, subsetTrainWithNames)
 
-# The following will subset only measurements for the mean and standard deviation.
+## Add column names.
+colnames(combinedDataset)[1] <- "subject"
+colnames(combinedDataset)[2] <- "activity"
+colnames(combinedDataset)[3:(ncol(combinedDataset))] <- featureVector[allCols]
 
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
+## Summarize the data by name and activity caluculating the mean of the columns.
+data <- aggregate(combinedDataset, by=list(combinedDataset$subject, 
+                                                  combinedDataset$activity), FUN=mean, na.rm=TRUE)
+## Remove the first two columns, which are redundant to the third and fourth.
+data <- data[, 3:(ncol(data))]
 
-# Convert the column numbers to a vector of variable names matching columns in `dt`.
+## Convert the activity data into human-readable factors.
+data[,2] <- factor(data[,2])
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+levels(data[,2]) <- activities[,2]
 
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-head(dtFeatures)
-dtFeatures$featureCode
-
-# Subset these variables using variable names.
-
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with=FALSE]
-
-
-#3. Uses descriptive activity names to name the activities in the data set
-
-# Tips: the file 'activity_labels.txt'can be used to add descriptive names to the activities.
-
-dtActivityNames <- fread(file.path(pathIn, "activity_labels.txt"))
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
-
-#4. Apropriately labels the data set with descriptive activity names. 
-
-# The following will merge the activity names
-dt <- merge(dt, dtActivityNames, by="activityNum", all.x=TRUE)
-
-# Add 'activityName as a key.
-
-setkey(dt, subject, activityNum, activityName)
-
-# Melt the data table to reshape it from a short and wide format to a tall and narrow format.
-
-dt <- data.table(melt(dt, key(dt), variable.name="featureCode"))
-
-# Merge activity name.
-
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by="featureCode", all.x=TRUE)
-
-# Create a new variable, `activity` that is equivalent to `activityName` as a factor class.
-# Create a new variable, `feature` that is equivalent to `featureName` as a factor class.
-
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
-
-# Seperate features from `featureName` using the helper function `grepthis`.
-
-grepthis <- function (regex) {
-  grepl(regex, dt$feature)
-}
-## Features with 2 categories
-n <- 2
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol=nrow(y))
-dt$featDomain <- factor(x %*% y, labels=c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol=nrow(y))
-dt$featInstrument <- factor(x %*% y, labels=c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol=nrow(y))
-dt$featAcceleration <- factor(x %*% y, labels=c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol=nrow(y))
-dt$featVariable <- factor(x %*% y, labels=c("Mean", "SD"))
-## Features with 1 category
-dt$featJerk <- factor(grepthis("Jerk"), labels=c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), labels=c(NA, "Magnitude"))
-## Features with 3 categories
-n <- 3
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol=nrow(y))
-dt$featAxis <- factor(x %*% y, labels=c(NA, "X", "Y", "Z"))
-
-#5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.  
-
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by=key(dt)]
-head(dtTidy)
-write.table(dtTidy, "tidy.txt", quote = FALSE, row.names = FALSE)
+## Create the output file to upload.
+write.table(data, "./tidy.txt")
